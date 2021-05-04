49c204905cf7e330259af7e5b1d1284c08a55d28
clementpaul112 <72744246+clementpaul112@users.noreply.github.com>
2021-05-04 22:22:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+Webcam.set(
+    {
+        width : 350,
+        height : 350,
+        image_format: 'png',
+        png_quality : 90
+    }
+    );
+    camera = document.getElementById("camera");
+    Webcam.attach('#camera');
+function take_snapshot()
+   {
+    Webcam.snap(function (data_uri)
+       {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+       });
+   };
+
+   console.log('ml5 version:', ml5.version);
+
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/model.json',modelLoaded);
+    
+
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotresult);
+    
+}
+
+function gotresult(error, results)
+{
+   if (error)
+   {
+      console.error(error);
+   } else
+   {
+      console.log(results);
+      document.getElementById("result_person_name").innerHTML = results[0].label;
+      document.getElementById("result_person_accuracy").innerHTML = results[0].confidence.toFixed(3);
+   }
+}
